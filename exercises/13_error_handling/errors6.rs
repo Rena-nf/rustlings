ca91f331283399d4c58a8644c85eb57415281e37
Rena-nf <thenamealreadytakentwice@gmail.com>
2024-10-01 11:57:23 +0700

    Finished Error handling and Generics

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -26,6 +26,9 @@ impl ParsePosNonzeroError {
 
     // TODO: Add another error conversion function here.
     // fn from_parse_int(???) -> Self { ??? }
+    fn from_parse_int(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -40,11 +43,12 @@ impl PositiveNonzeroInteger {
         }
     }
 
-    fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
-        // TODO: change this to return an appropriate error instead of panicking
-        // when `parse()` returns an error.
-        let x: i64 = s.parse().unwrap();
-        Self::new(x).map_err(ParsePosNonzeroError::from_creation)
+    fn parse(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
+        let x: i64 = s.parse()
+            .map_err(ParsePosNonzeroError::from_parse_int)?;
+    
+        PositiveNonzeroInteger::new(x)
+            .map_err(ParsePosNonzeroError::from_creation)
     }
 }
 
